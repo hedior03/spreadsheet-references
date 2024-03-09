62a160e9915d05fe0fc12116641d4a25bf674117
Hernan Diaz <hernandiaz035@gmail.com>
2024-03-09 13:46:44 +1100

    fix: allow files with comma and not space after

--- src/getSpreadsheet.ts
+++ src/getSpreadsheet.ts
@@ -1,4 +1,4 @@
 export const getSpreadsheet = async (filename: string): Promise<string[][]> => {
   const text = (await Deno.readTextFile(filename)).trim();
-  return text.split("\n").map((col) => col.split(/,\s+/));
+  return text.split("\n").map((col) => col.split(/,\s*/));
 };
